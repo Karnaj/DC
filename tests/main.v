22476583ea1f42e801e10c528b90d3cf493fa963
Karnaj <yoan.geran@hotmail.fr>
2021-04-06 12:46:04 +0200

    examples of tests.

---
+++ tests/main.v
@@ -0,0 +1,19 @@
+(* This script tests the Dedukti plugin by exporting test files and parts
+   of the Coq standard library. *)
+
+Declare ML Module "coqine_plugin".
+
+Set Printing Universes.
+
+Dedukti Set Destination "out".
+
+Dedukti Enable Debug.
+Dedukti Set Debug "debug.out".
+
+(*Dedukti Add Debug "Coq.Init.Datatype". *)
+
+Require Import import.
+
+Load config.
+
+Dedukti Export All.
